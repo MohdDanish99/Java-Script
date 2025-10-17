562f012bd9cf6ed6bfd80701a72078b09f14fb87
Mohd Danish <04.mohd.danish.08@gmail.com>
2025-10-17 06:47:09 +0000

    veriable learning

---
+++ Basic-JS/variable.js
@@ -0,0 +1,12 @@
+const accountId=259110; // const means is the hold the unique value
+let accountEmail="ddhddid284@gmail.com";
+var accountName="5933"; //we dont use this because in this varible the ffunction scope are not define;
+accountCity="jaipur"  // we are declayer the any value without the given anytype of the variable.
+// testing the how many variable are changes or not;
+//   const accountId=23;
+ accountEmail="hdmi23@gmail.com"; // newone are print.
+ accountName="433";
+ accountCity="Bengluru";
+
+ console.log(accountId);//if you went to the single value to print 
+ console.table([accountId,accountEmail,accountName,accountCity]) //if you went to the table of the data then you used this variable
